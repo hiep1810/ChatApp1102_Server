bee20d4b5a6b079f3cd38e8635450ca544af1150
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2020-05-30 16:44:01 +0700

    last commit

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  MONGO: 'mongodb://localhost:27017/chatapp_1102'
+  MONGO:
+    'mongodb+srv://admin:hiepdt98@cluster0-fdeqn.mongodb.net/test?retryWrites=true&w=majority',
+  //'mongodb://localhost:27017/chatapp_1102'
 };
